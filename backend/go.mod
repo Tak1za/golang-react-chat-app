c6dcc4a04b1fcbd8e7c8a51f457e5b59073e48b6
Varun Gupta <varun.gupta3@honeywell.com>
2020-04-25 02:42:48 +0530

    Refactored backend

--- backend/go.mod
+++ backend/go.mod
@@ -2,4 +2,4 @@ module chat-app/backend
 
 go 1.13
 
-require github.com/gorilla/websocket v1.4.2 // indirect
+require github.com/gorilla/websocket v1.4.2
--- backend/main.go
+++ backend/main.go
@@ -1,52 +1,22 @@
 package main
 
 import (
+	"chat-app/backend/websocket"
 	"fmt"
 	"log"
 	"net/http"
-
-	"github.com/gorilla/websocket"
 )
 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-	CheckOrigin:     func(r *http.Request) bool { return true },
-}
-
-func reader(conn *websocket.Conn) {
-	for {
-		messageType, p, err := conn.ReadMessage()
-		if err != nil {
-			log.Println(err)
-			return
-		}
-
-		fmt.Println(string(p))
-
-		if err := conn.WriteMessage(messageType, p); err != nil {
-			log.Println(err)
-			return
-		}
-	}
-}
-
 func serveWs(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.Host)
-
-	ws, err := upgrader.Upgrade(w, r, nil)
+	ws, err := websocket.Upgrade(w, r)
 	if err != nil {
 		log.Println(err)
 	}
-
-	reader(ws)
+	go websocket.Writer(ws)
+	websocket.Reader(ws)
 }
 
 func setupRoutes() {
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(rw, "Simple server")
-	})
-
 	http.HandleFunc("/ws", serveWs)
 }
 
